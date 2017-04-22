9dd1938f793b0019ce660a88842bc258ad4d650c
mph <delusionalfrog@gmail.com>
2017-04-23 01:43:18 +0800

    add model h

--- randomforest/model.h
+++ randomforest/model.h
@@ -1,20 +1,32 @@
 //model struct api
 #include "data.h"
+#include <fstream>
 
 struct RFNode{
 	int featureId;
 	float threshold;
-	RFNode* left, right;
+	RFNode* left, *right;
 	RFNode(): featureId(-1), threshold(0), left(NULL), right(NULL) {}
 };
 
 class RFModel{
 private:
-	vector<RFModel* > Trees;
+	std::vector<RFNode* > Trees;
+	void outputOneTree(std::ofstream& out, RFNode * Tree, int deep);
+	void insertTab(std::ofstream& out, int deep);
+	float getValue(std::string s);
+	RFNode* trainOneTree(DataRF * data);
+	RFNode* trainChildTree(std::vector<int> &types, std::vector<std::vector<float> > &values, std::vector<int> usefeature, int lastfeature);
+	
+	int treeNum = 10;
+	float sampleFeatureRate = 0.3;
+	float minleaf = 10;
+	
 public:
 	bool loadModel(char *file);
 	bool outputModel(char *file);
 	void trainModel(DataRF * data);
 	void initSimpleModel();
 	int predict(std::vector<float> inputData);
+	void setTrainPara(int treeNum, float sampleFeatureRate);
 };
